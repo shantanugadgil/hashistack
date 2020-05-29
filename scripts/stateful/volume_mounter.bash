d7a2db62abd32cb93b3c4870e610442bc3e45ac5
Shantanu Gadgil <shantanugadgil@users.noreply.github.com>
2020-05-30 04:19:26 +0530

    Update volume_mounter.bash

--- scripts/stateful/volume_mounter.bash
+++ scripts/stateful/volume_mounter.bash
@@ -5,13 +5,12 @@ set -x
 
 exec 1>> /tmp/volume_mounter.log 2>&1
 
-device_path="/dev/xvdc"
-
 region=$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 
 instance_id=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
 echo "ASG_INDEX [$ASG_INDEX]"
+echo "DEVICE_PATH [$DEVICE_PATH]"
 echo "instance [$instance_id]"
 
 volume_id=$(aws --region $region ec2 describe-volumes --filters --filters Name=tag:Name,Values=worker-$ASG_INDEX | jq -r '.Volumes[].VolumeId')
@@ -30,26 +29,21 @@ while (( 1 )); do
     sleep 30
 done
 
-aws --region $region ec2 attach-volume --volume-id $volume_id --instance-id $instance_id --device $device_path
+aws --region $region ec2 attach-volume --volume-id $volume_id --instance-id $instance_id --device $DEVICE_PATH
 sleep 10
 
 mkdir -p /data
 
-blkid $device_path
+blkid $DEVICE_PATH
 ret=$?
 
 if (( $ret != 0 )); then
-        mkfs.ext4 $device_path
+        mkfs.ext4 $DEVICE_PATH
 fi
 
-mount /dev/xvdc /data
+mount -v $DEVICE_PATH /data
 
-date >> /data/log.txt
-
-###
-
-sudo systemctl daemon-reload
-sudo systemctl enable consul nomad
-sudo systemctl restart consul nomad
+dd=$(date)
+echo "[$dd] mount and startup message" >> /data/log.txt
 
 echo "Done"

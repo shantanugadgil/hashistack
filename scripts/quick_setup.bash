21c34d795d25e160aed0753df95402b225ec23ec
Shantanu Gadgil <shantanugadgil@users.noreply.github.com>
2021-12-16 19:31:19 +0530

    Update quick_setup.bash

--- scripts/quick_setup.bash
+++ scripts/quick_setup.bash
@@ -6,7 +6,7 @@ set -u
 set -e
 
 ###############################################################################
-CONSUL_VERSION=${CONSUL_VERSION:-"1.10.4"}
+CONSUL_VERSION=${CONSUL_VERSION:-"1.11.1"}
 
 consul_config_dir="/etc/consul.d"
 consul_server_config="${consul_config_dir}/server.hcl"
@@ -16,7 +16,7 @@ consul_systemd_file="/etc/systemd/system/consul.service"
 consul_upstart_file="/etc/init/consul.conf"
 
 ###############################################################################
-NOMAD_VERSION=${NOMAD_VERSION:-"1.2.2"}
+NOMAD_VERSION=${NOMAD_VERSION:-"1.2.3"}
 
 nomad_config_dir="/etc/nomad.d"
 nomad_common_config="${nomad_config_dir}/common.hcl"
@@ -28,7 +28,7 @@ nomad_systemd_file="/etc/systemd/system/nomad.service"
 nomad_upstart_file="/etc/init/nomad.conf"
 
 ###############################################################################
-VAULT_VERSION=${VAULT_VERSION:-"1.9.0"}
+VAULT_VERSION=${VAULT_VERSION:-"1.9.1"}
 
 vault_config_dir="/etc/vault.d"
 vault_server_config="${vault_config_dir}/server.hcl"
